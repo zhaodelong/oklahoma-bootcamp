53a17622f2e2cbde4a55d309936d74c471787faf
Delong Zhao <zhaodelong@gmail.com>
2013-07-01 14:35:45 -0500

    creat branch

--- add_nums.py
+++ add_nums.py
@@ -1,12 +1,17 @@
 # tHIS HAS BEEN AUTHORED BY DELONG
 
+from adding import add_three
+
+
 def add_two(a,b):
    return a+b
 
 def main():
    print ("2 + 3 = ",add_two(2,3))
    print ("2 + 4 = ",add_two(2,4))
+   print ("2 + 3 + 4 = ",add_three(2,3,4))
 if __name__ == "__main__":
      main()
+
 
 
